2f6cc637cb663520f0020f1615647bb21252f545
Justin Chang <jchang691@gmail.com>
2013-01-13 00:23:26 -0600

    Adds pagination and ajax to the search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,11 @@ class SearchController < ApplicationController
 
     def search
         @user = current_user
+        movies_per_page = @user.nil? ? 10 : @user.movies_per_page
         @search_query = params[:search]
         @movies = Movie.find(:all, :conditions => ['lower(name) LIKE ?', "%#{@search_query.downcase}%"])
-        @movies = @movies.paginate(:order=> "name", page: params[:page]) unless @movies.empty?
+        @movies = @movies.paginate(:order=> "name", page: params[:movie_page], :per_page => movies_per_page) unless @movies.empty?
         @actors = Actor.find(:all, :conditions => ['lower(name) LIKE ?', "%#{@search_query.downcase}%"])
-        @actors = @actors.paginate(:order=> "name", page: params[:page]) unless @actors.empty?
+        @actors = @actors.paginate(:order=> "name", page: params[:actor_page], :per_page => movies_per_page) unless @actors.empty?
     end
 end
